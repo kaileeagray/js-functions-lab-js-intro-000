950398f5911fd8eb28e0cbac631ec993915ace9d
Kailee Gray <kaileeagray@gmail.com>
2019-04-30 17:17:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 // Write a function named happyHolidays. This function should not accept any parameters and should return the string "Happy holidays!".
+
+
 //
 // Write a function named happyHolidaysTo. This function should accept a parameter of the name of the person you want to wish happy holidays, and return the string `Happy holidays, ${name}!`
 //
